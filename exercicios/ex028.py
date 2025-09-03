bf04f4f8f341a68ef8314abd6a62bb224a3df6bb
Daniel <diasdaniel744@gmail.com>
2025-09-03 16:22:15 -0300

    Adicionando exercícios e aula nova

---
+++ exercicios/ex028.py
@@ -0,0 +1,2 @@
+# Fazer um progrmaa que faça o computador pensar em um número inteiro entre 0 e 5 e peça para o usuário descobrir qual foi o número escolhido pelo computador
+# O programa deverá escrever na tela se o usuário ganhou ou venceu
