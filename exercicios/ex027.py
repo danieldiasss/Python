b9d63a8ab1c2ac7151ce19dd445d5707eefc0802
Daniel <diasdaniel744@gmail.com>
2025-09-03 13:38:44 -0300

    Adding exercise 027.py

--- exercicios/ex027.py
+++ exercicios/ex027.py
@@ -1,2 +1,7 @@
 # Faça um programa que leia o nome completo de uma pessoa e mostre em seguida o primeiro e ultimo nome
 # Ex: Daniel Dias Ribeiro    ,   Daniel Ribeiro
+
+n = str(input("Digite o seu nome completo: ")).strip()
+nome = n.split()
+print("Seu primeiro nome é {}" .format(nome[0]))
+print("Seu último nome é {}" .format(nome[-1]))
