78b89d34eccb8083c28c126d68d3033ed56b08f9
Daniel <diasdaniel744@gmail.com>
2025-09-03 17:37:15 -0300

    adicionando resolução dos exercícios 33,34,35

--- exercicios/ex034.py
+++ exercicios/ex034.py
@@ -3,3 +3,9 @@
 # Para salários superiores a R$1.250,00, calcule um aumento de 10%.
 
 # Para os inferiores ou iguais, o aumento é de 15%.
+
+salario = int(input("Digite seu salário: "))
+if salario <= 1250:
+    print("O seu novo salário é {:.2f}" .format(salario + salario *0.15))
+else:
+    print("O seu novo salário é {:.2f}" .format(salario + salario * 0.10))
--- exercicios/ex035.py
+++ exercicios/ex035.py
@@ -1,1 +1,8 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+print("-=-"*20)
+print("Analisador de triângulos")
+print("-=-"*20)
+a = float(input("Digite o primeiro segmento: "))
+b = float(input("Digite o segundo segmento: "))
+c = float(input("Digite o terceiro segmento: "))
+print("Pode ser um triângulo" if a+b>c and a+c > b and b+c>a else "Não pode ser um triângulo")
