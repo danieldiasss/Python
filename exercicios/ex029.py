9ebfc14af859c1a87dcfb420ff2653a0ac273427
Daniel <diasdaniel744@gmail.com>
2025-09-03 16:41:59 -0300

    Adicionando resolução do exercício 029

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -1,3 +1,9 @@
 # Escreva um programa que leia a velocidade de um carro.
 # Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
 # A multa vai custar R$7,00 por cada Km acima do limite.
+vel = int(input("Qual a velocidade do carro? "))
+multa = (vel - 80) * 7
+if vel >80:
+    print("O valor da sua multa será {:.2f}R$" .format(multa) )
+else:
+    print("Você não foi multado")
