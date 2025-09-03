d883c8ebb68f5b66d8181958bedbbcf6806c5bb9
Daniel <diasdaniel744@gmail.com>
2025-09-03 16:54:30 -0300

    Adicionando resolução dos exercícios 30,31,32

--- exercicios/ex031.py
+++ exercicios/ex031.py
@@ -1,1 +1,6 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
+viagem = int(input("Digite a distância da viagem: "))
+if viagem > 200:
+    print("O valor da passagem é {:.2f} R$" .format(viagem*0.45))
+else:
+    print("O valor da viagem é {:.2f} R$"   .format(viagem*0.5))
--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -1,1 +1,8 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto
+ano = int(input("Digite um ano: "))
+if ano % 4 == 0:
+    print("Esse ano é bissexto")
+else:
+    print("Esse ano não é bissexto")
+
+# print("Esse ano é bissexto" if ano % 4 == 0 else "Esse ano não é bissexto") # forma de resolver por condição simplificada
