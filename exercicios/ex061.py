f04a941cb46b3a98897f7c07c24e43df4e05c180
Daniel <diasdaniel744@gmail.com>
2025-09-08 19:41:49 -0300

    Adicionando ex061-062

--- exercicios/ex061.py
+++ exercicios/ex061.py
@@ -1,1 +1,10 @@
 # Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA. mostrando os 10 primeiros termos da progressão usando a estrutura while.
+n1 = int(input("Digite o primeiro termo: "))
+r = int(input("Digite a razão: "))
+cont = 1
+termo = n1
+while cont <= 10:
+    print(f" {termo} --> " , end = "")
+    termo += r
+    cont += 1
+print("Fim")
--- exercicios/ex062.py
+++ exercicios/ex062.py
@@ -1,1 +1,19 @@
 #  Melhora o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.
+print("GERADOR DE PROGRESSÃO ARITMÉTICA")
+print("-=-" *20)
+n1 = int(input("Digite o primeiro termo: "))
+razao = int(input("Digite a razão: "))
+
+total = 0
+mais = 10
+termo = n1
+cont = 1
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print(f"{termo}--> " , end = "" )
+        cont += 1
+        termo += razao
+    print("Pausa")
+    mais = int(input("Quantos termos você quer mostrar mais? "))
+print("Fim")
