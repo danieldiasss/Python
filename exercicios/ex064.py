d34ef58784f28051e3dbf902809b21665eb5c709
Daniel <diasdaniel744@gmail.com>
2025-09-09 13:59:31 -0300

    Adicionando resolução dos ex064-065

--- exercicios/ex064.py
+++ exercicios/ex064.py
@@ -1,1 +1,16 @@
-#Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
+# Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário 
+# digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e 
+# qual foi a soma entre eles (desconsiderando o flag).
+print("SOMA DE VÁRIOS NÚMEROS")
+print("-=-" *30 )
+
+num = 0
+valor = 0
+
+print("Digite 999 para sair")
+print("-=-" *30 )
+while valor != 999:
+    valor = int(input("Digite um número: "))
+    num = num + valor
+
+print(f"A soma dos números foi {num-999}")
--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,1 +1,34 @@
-# Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+# Crie um programa que leia vários números inteiros pelo teclado. No final da execução,
+# mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. 
+# O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+print("LEITOR DE NÚMEROS")
+print("-=-"*30)
+
+maior = 0
+menor = 0
+soma_idade= 0
+cont = 0
+continuar = ""
+while continuar != "N":
+    num = float(input("Digite um número: "))
+
+    cont += 1
+    soma_idade += num
+    media = soma_idade/cont
+    
+    if cont == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    continuar = str(input("Deseja continuar? [S/N]")).upper().strip()[0]
+
+print(f''' 
+A média dos números deu {media}
+O maior número é {maior}
+O menor número é {menor}
+''')
+
