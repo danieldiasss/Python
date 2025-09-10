14d2a43dbd73519bab0098578d43cee6472319ac
Daniel <diasdaniel744@gmail.com>
2025-09-09 21:20:47 -0300

    Adicionando resolução dos exercícios 066-068

--- exercicios/ex067.py
+++ exercicios/ex067.py
@@ -1,1 +1,13 @@
-#  Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+#  Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicit
+
+
+while True:
+    num = (int(input("Quer ver qual tabuada? [Negativo para parar: ")))
+    if num < 0:
+        break
+    c = 0
+    while c < 10:
+        c += 1
+        tabuada = num * c
+        print(f"{c} x {num}: {tabuada}") 
+print("Programa finalizado")
