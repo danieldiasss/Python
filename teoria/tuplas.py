707862bd5f75dfb4bbe9911817e7a1b15ad074b2
Daniel <diasdaniel744@gmail.com>
2025-09-10 18:40:42 -0300

    Adicionando tuplas e ex072-077

---
+++ teoria/tuplas.py
@@ -0,0 +1,28 @@
+# TUPLAS SÃO IMUTÁVEIS
+# lanche = ('Hambúrguer','suco','pizza','pudim')
+# for comida in lanche:
+#     print(f"Eu vou comer {comida}")
+    
+# for cont in range (0,len(lanche)):
+#     print(f"Eu vou comer {lanche[cont]} na posição {cont}")
+
+# for pos,comida in enumerate (lanche):
+#     print(f"Eu vou comer {comida} na posição {pos}")
+# print("Comida pra caramba")
+
+# lanche = ('Hambúrguer','suco','pizza','pudim')
+# print(sorted(lanche))
+
+# a = (2,5,4)
+# b =(5,8,1,2)
+# c = a+b
+# print(c)
+# print(len(c))
+# print(c.count(2)) # Quantas vezes o 2 apareceu
+# print(c.index(8)) # Qual posição está o 8
+# print(c.index(5,1)) # pega o 5 a partir da posição 1
+
+
+pessoa = ('Daniel', 39, 'M', 75.5)
+del pessoa # Isso apaga a tupla por inteira, mas n tem como mudar um valor específico
+print(pessoa)
