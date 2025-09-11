7029709e2d5fe05c6ce8cef0b1cc7c795b3a83b5
Daniel <diasdaniel744@gmail.com>
2025-09-11 10:42:11 -0300

    Adicionando resolução ex077

--- exercicios/ex077.py
+++ exercicios/ex077.py
@@ -1,3 +1,9 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 palavras = ('cachorro', 'gato', 'passaro', 'peixe', 'leao', 'tigre', 'elefante')
+
+for p in palavras:
+    print(f"\nA palavra {p} possui as vogais: ", end="")
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=" ")
