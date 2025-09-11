be37cb4b76f6a9f75123894eb12c4b10c7382824
Daniel <diasdaniel744@gmail.com>
2025-09-11 12:56:48 -0300

    Adicionando listas2.py e ex078-083

---
+++ teoria/listas2.py
@@ -0,0 +1,47 @@
+# # Listas são entre colchetes[], enquanto as tuplas entre parênteses ()
+# # Listão são mutáveis
+# num = [2,5,9,1]
+# num[2] = 3   # troca elemento de uma lista
+# num.append(7) # adiciona elemento à uma lista
+# num.sort()   # deixa de forma crescente
+# num.sort(reverse=True)  # deixa de forma decrescente
+# num.insert(2,0)  # na posição irá inserir o 0
+# num.pop()   # elimina o último valor
+# num.pop(3)  # elimina o 3 elemento da lista que 
+# num.remove(2) # elimina o primeiro valor 2 da lista, se não tiver o valor da lista dá erro
+
+# print(num)
+# print(f"Essa lista tem {len(num)} elementos")
+
+
+
+# valores = []
+# valores.append(5)
+# valores.append(9)
+# valores.append(4)
+
+# for c, v in enumerate(valores):     # c é a chave(onde está localizada)   v é o valor (1,2,3,4,5,6.....)
+#     print(f'Na posição {v} encontrei o valor {c}')
+# print("Cheguei ao final da lista")
+
+
+
+# valores = list()
+# for cont in range (0,5):
+#     valores.append(int(input("Digite um valor: ")))
+# for c, v in enumerate(valores):
+#     print(f"Na posição {c} encontrei o valor {v}")
+# print("="*30)
+# print("Cheguei ao final da lista")
+
+# a = [2,3,5,7]
+# b = a
+# b[2] = 8           
+# print(f"Lista {a}")
+# print(f"Lista {b}")       # a partir do momento que você iguala uma lista na outra, as duas ficam ligadas
+
+a = [2,3,5,7]
+b = a [:]             # faz uma cópia
+b[2] = 8           
+print(f"Lista {a}")
+print(f"Lista {b}") 
