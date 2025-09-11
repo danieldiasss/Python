475252a6759a384d0cf6840de982c2a123087107
Daniel <diasdaniel744@gmail.com>
2025-09-11 13:31:26 -0300

    Adicionando resolução do ex081

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -7,3 +7,21 @@
 # B) A lista de valores, ordenada de forma decrescente.
 
 # C) Se o valor 5 foi digitado e está ou não na lista.
+
+lista_numeros = []
+continuar = ""
+cont = 0
+while continuar != "N":
+    numeros =  int(input("Digite um número: "))
+    continuar = str(input("Deseja continuar? [S/N]")).strip().upper()[0]
+    cont+=1
+    lista_numeros.append(numeros)
+    lista_numeros.sort(reverse=True)
+    
+print(f"Foram digitados {cont} números")
+print(f"A lista de valores de forma decrescente é {lista_numeros}")
+if 5 in lista_numeros:
+    print(f"O número 5 foi digitado")
+else:
+    print(f"O número 5 não foi digitado")
+print("ENCERROU")
