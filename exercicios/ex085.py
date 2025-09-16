0f95906bdec0515a4f577eee996d778c9555d253
Daniel <diasdaniel744@gmail.com>
2025-09-16 12:45:48 -0300

    Atualizando listas2 e adicionando novos exercícios

---
+++ exercicios/ex085.py
@@ -0,0 +1,3 @@
+# Faça um programa onde o usuário possa digitar 7 valores númericos e cadastre-os em uma 
+# lista única que mantenha separado os valores pares e ímpares
+# no final mostre os valores pares e ímpares em ordem crescente
--- teoria/listas2.py
+++ teoria/listas2.py
@@ -40,8 +40,46 @@
 # print(f"Lista {a}")
 # print(f"Lista {b}")       # a partir do momento que você iguala uma lista na outra, as duas ficam ligadas
 
-a = [2,3,5,7]
-b = a [:]             # faz uma cópia
-b[2] = 8           
-print(f"Lista {a}")
-print(f"Lista {b}") 
+# a = [2,3,5,7]
+# b = a [:]             # faz uma cópia
+# b[2] = 8           
+# print(f"Lista {a}")
+# print(f"Lista {b}") 
+
+
+# teste = list()
+# teste.append('Daniel')
+# teste.append(40)
+
+# galera = list()
+# galera.append(teste[:])
+
+# teste [0] = 'Maria'
+# teste [1] = 22
+# galera.append(teste[:])
+# print(galera)
+
+# galera = [['Joao', 19] , ['Ana' , 33] , ['Joaquim' , 13] , ['Maria', 45]]
+# print(galera[2] [1])
+
+# for p in galera:
+#     print(f'{p[0]} tem {p[1]} anos de idade.')
+
+galera = list()
+dado = list()
+totmai = totmen = 0
+for c in range (0,3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+print(galera)
+
+for p in galera:
+    if p[1]>= 21:
+        print(f'{p[0]} é maior de idade.')
+        totmai += 1
+    else:
+        print(f'{p[0]} é menor de idade')
+        totmen += 1
+print(f"Temos {totmai} maiores e {totmen} menores de idade")
